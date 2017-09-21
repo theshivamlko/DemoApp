442682a8d9b4ef4298c61c9d9fdf6964a2ca94d1
Shivam <shivamsrivastava.9@gmail.com>
2017-09-21 22:45:06 +0530

    21/9/2017

---
+++ app/src/main/java/in/shivam/navoki/demoapp/Layouts.java
@@ -0,0 +1,14 @@
+package in.shivam.navoki.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouts);
+
+    }
+}
--- app/src/main/java/in/shivam/navoki/demoapp/MainPage.java
+++ app/src/main/java/in/shivam/navoki/demoapp/MainPage.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainPage extends Activity{
 
     Context context;
+    private CheckBox checBox;
+    private RadioGroup radioGroup;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +31,11 @@ public class MainPage extends Activity{
 
         Button btn6=(Button)findViewById(R.id.button6);
         Button btn7=(Button)findViewById(R.id.button7);
+
+        final EditText editText=(EditText) findViewById(R.id.ediText1);
+        final EditText editText2=(EditText) findViewById(R.id.ediText2);
+        checBox = (CheckBox) findViewById(R.id.checBox);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
 
 
         btn2.setOnClickListener(onClickListener);
@@ -39,6 +51,44 @@ public class MainPage extends Activity{
                 Toast.makeText(context,"CLICK 5",Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               String n1=editText.getText().toString();
+               String n2=editText2.getText().toString();
+                // "22" +"30"  ""25.2"
+               int a=Integer.parseInt(n1);//
+               int b=Integer.parseInt(n2);
+               int sum=a+b;
+
+               Toast.makeText(context,"Totol ="+sum,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        checBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(context,"STATE ="+b,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                if (i == R.id.radio1)
+                    Toast.makeText(context, "MALE", Toast.LENGTH_SHORT).show();
+                else if (i == R.id.radio2)
+                    Toast.makeText(context, "FEMALE", Toast.LENGTH_SHORT).show();
+
+            }
+
+            });
+
 
 
     }
